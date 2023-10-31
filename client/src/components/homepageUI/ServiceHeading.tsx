71d75fe05ebed09f35877a896b6e0f32f8cd91d5
ujjalzaman <muzzwal97@gmail.com>
2023-10-31 21:53:16 +0600

    Updated UI of Service Heading

--- client/src/components/homepageUI/ServiceHeading.tsx
+++ client/src/components/homepageUI/ServiceHeading.tsx
@@ -5,17 +5,17 @@ import leptopImage from '@/assets/homepage/leptop.png';
 
 const ServiceHeading = () => {
     return (
-        <div className="mb-5 mx-5" style={{marginTop:"-5rem"}}>
+        <div className="mb-5 mx-5" style={{ marginTop: "-5rem" }}>
             <div className="row">
                 <div className="col">
                     <div className="card shadow text-center">
                         <div>
                             <Image src={dImage} className="" width={220} alt="Fissure in Sandstone" />
                         </div>
-                        <div className="card-body bg-warning">
-                            <h5 className="card-title">FREE DIAGNOSTICS.</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                            <a href="" className="btn btn-primary w-50">Read More</a>
+                        <div className="card-body bg-secondary">
+                            <h5 className="card-title text-white">FREE DIAGNOSTICS.</h5>
+                            <p className="card-text text-white">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <a href="/" className="btn btn-primary w-50">Read More</a>
                         </div>
                     </div>
                 </div>
@@ -25,9 +25,9 @@ const ServiceHeading = () => {
                         <div>
                             <Image src={helloImpage} className="" width={220} alt="Fissure in Sandstone" />
                         </div>
-                        <div className="card-body bg-warning">
-                            <h5 className="card-title">ONLINE HELP.</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                        <div className="card-body bg-secondary">
+                            <h5 className="card-title text-white">ONLINE HELP.</h5>
+                            <p className="card-text text-white">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
                             <a href="/" className="btn btn-primary w-50">Read More</a>
                         </div>
                     </div>
@@ -38,17 +38,15 @@ const ServiceHeading = () => {
                         <div>
                             <Image src={leptopImage} className="" width={220} alt="Fissure in Sandstone" />
                         </div>
-                        <div className="card-body bg-warning">
-                            <h5 className="card-title">PROFESSIONAL REPAIR.</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                        <div className="card-body bg-secondary">
+                            <h5 className="card-title text-white">PROFESSIONAL REPAIR.</h5>
+                            <p className="card-text text-white">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
                             <a href="/" className="btn btn-primary w-50">Read More</a>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
     )
 }
 
